9c3bc4846deb95b62b0b5faa26a81a603df07f50
Jason Sanford <jasonsanford@gmail.com>
2017-04-05 08:51:03 -0400

    Remove old servers

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,3 @@
-// export const server = 'http://localhost:3000/';
-// export const server = 'https://457bc7ad.ngrok.io/';
 export const server = 'https://api.fulcrumapp.com/';
 
 const lastSlashIndex = window.location.pathname.lastIndexOf('/');
