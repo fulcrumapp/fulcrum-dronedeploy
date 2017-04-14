f1e826e27e4d986cebe4ab685b9bbfc5f51c6978
Jason Sanford <jasonsanford@gmail.com>
2017-04-14 13:52:47 -0400

    Make forms in form picker searchable

--- app/components/FormPicker.jsx
+++ app/components/FormPicker.jsx
@@ -14,6 +14,23 @@ export default class FormPicker extends React.Component {
     super(props);
 
     this.handleFormClicked = this.handleFormClicked.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+
+    this.sortedForms = props.forms.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+
+      if (b.name > a.name) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    this.state = {
+      filteredForms: this.sortedForms
+    };
   }
 
   render() {
@@ -23,31 +40,41 @@ export default class FormPicker extends React.Component {
           <p>Select a form to sync annotations.</p>
         </div>
         <div className="row">
-          {this.props.forms
-            .sort((a, b) => {
-              if (a.name > b.name) {
-                return 1;
-              }
-
-              if (b.name > a.name) {
-                return -1;
-              }
-
-              return 0;
-            })
-            .map((form, i) => {
-              return (
-                <button key={i}
-                  onClick={() => this.handleFormClicked(form)}>
-                  {form.name}
-                </button>
-              );
-            })
-          }
+          <div className="input-field col-4">
+            <input
+              onChange={this.handleSearchChange}
+              type="text" />
+            <label htmlFor="email">Search Forms</label>
+          </div>
+        </div>
+        <div className="row">
+          {this.state.filteredForms.map((form, i) => {
+            return (
+              <button key={i}
+                onClick={() => this.handleFormClicked(form)}>
+                {form.name}
+              </button>
+            );
+          })}
         </div>
         <Navigation />
       </div>
     );
+  }
+
+  handleSearchChange(event) {
+    const searchText = event.target.value.trim().toLowerCase();
+    let filteredForms = null;
+
+    if (searchText.length > 0) {
+      filteredForms = this.sortedForms.filter((form) => {
+        return form.name.toLowerCase().indexOf(searchText) > -1;
+      });
+    } else {
+      filteredForms = this.sortedForms;
+    }
+
+    this.setState({ filteredForms });
   }
 
   handleFormClicked(form) {
